da7ef8cc58018fc8e8fc90603f755a8e1167089a
fabiooo4 <fabioirimie@gmail.com>
2023-09-18 15:46:03 +0200

    added SnipRun mappings

--- after/plugin/sniprun.lua
+++ after/plugin/sniprun.lua
@@ -1,3 +1,5 @@
 require('sniprun').setup({
     live_mode_toggle='enable',
 })
+
+vim.keymap.set('n', '<F5>', ":let b:caret=winsaveview() <CR> | :%SnipRun <CR>| :call winrestview(b:caret) <CR>", {})
