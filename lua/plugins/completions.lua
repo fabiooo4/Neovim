470ac182fceae7dc34581b1ad230e94a36889274
fabiooo4 <fabioirimie@gmail.com>
2024-05-27 15:50:48 +0200

    added autosnippets

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -12,14 +12,16 @@ return {
   {
     "hrsh7th/nvim-cmp",
     config = function()
+      require("luasnip").config.setup({ enable_autosnippets = true })
       local cmp = require("cmp")
-
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      -- load snippets from directory
-      require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
+      local luasnip = require("luasnip")
 
       cmp.setup({
+        require("luasnip.loaders.from_vscode").lazy_load(),
+
+        -- load snippets from directory
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } }),
+
         snippet = {
           -- REQUIRED - you must specify a snippet engine
           expand = function(args)
@@ -32,11 +34,44 @@ return {
           -- documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              if luasnip.expandable() then
+                luasnip.expand()
+              else
+                cmp.confirm({
+                  select = true,
+                })
+              end
+            else
+              fallback()
+            end
+          end),
+
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-g>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-n>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,47 +1,54 @@
 return {
-	{
-		"williamboman/mason.nvim",
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"clangd",
-					"rust_analyzer",
-					"lua_ls",
-					"html",
-					"cssls",
-					"jsonls",
-					"marksman",
-					"r_language_server",
-					"tsserver",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    priority = 53,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    priority = 52,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "clangd",
+          "rust_analyzer",
+          "lua_ls",
+          "html",
+          "cssls",
+          "jsonls",
+          "marksman",
+          "r_language_server",
+          "tsserver",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    priority = 51,
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- setup lsp
-			lspconfig.clangd.setup({ capabilities = capabilities })
-			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.html.setup({ capabilities = capabilities })
-			lspconfig.cssls.setup({ capabilities = capabilities })
-			lspconfig.jsonls.setup({ capabilities = capabilities })
-			lspconfig.marksman.setup({ capabilities = capabilities })
-			lspconfig.r_language_server.setup({ capabilities = capabilities })
-			lspconfig.tsserver.setup({ capabilities = capabilities })
-			lspconfig.ltex.setup({ capabilities = capabilities })
+      -- setup lsp
+      lspconfig.clangd.setup({ capabilities = capabilities })
+      lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.cssls.setup({ capabilities = capabilities })
+      lspconfig.jsonls.setup({ capabilities = capabilities })
+      lspconfig.marksman.setup({ capabilities = capabilities })
+      lspconfig.r_language_server.setup({ capabilities = capabilities })
+      lspconfig.tsserver.setup({ capabilities = capabilities })
 
-			-- keybinds
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      -- keybinds
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
