dd2e9cb3a41ca40128f53f0e09108b8cdf662bf9
fabiooo4 <fabioirimie@gmail.com>
2024-05-27 08:13:15 +0200

    added autocompletion and snippets

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,50 +1,51 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          -- LSP
-          "clangd",
-          "rust_analyzer",
-          "lua_ls",
-          "html",
-          "cssls",
-          "jsonls",
-          "marksman",
-          "r_language_server",
-          "tsserver",
-          "ltex",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					-- LSP
+					"clangd",
+					"rust_analyzer",
+					"lua_ls",
+					"html",
+					"cssls",
+					"jsonls",
+					"marksman",
+					"r_language_server",
+					"tsserver",
+					"ltex",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- setup lsp
-      lspconfig.clangd.setup({})
-      lspconfig.rust_analyzer.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.r_language_server.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.ltex.setup({})
+			-- setup lsp
+			lspconfig.clangd.setup({ capabilities = capabilities })
+			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
+			lspconfig.jsonls.setup({ capabilities = capabilities })
+			lspconfig.marksman.setup({ capabilities = capabilities })
+			lspconfig.r_language_server.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.ltex.setup({ capabilities = capabilities })
 
-      -- keybinds
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			-- keybinds
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
