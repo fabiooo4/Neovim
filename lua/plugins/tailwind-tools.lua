c6300c1b680b9bcd5cd500c347f90f0cba77d162
fabiooo4 <fabioirimie@gmail.com>
2024-07-28 00:55:06 +0200

    added tailwind-tools plugin

---
+++ lua/plugins/tailwind-tools.lua
@@ -0,0 +1,10 @@
+return {
+  "luckasRanarison/tailwind-tools.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    conceal = {
+      enabled = true,
+      min_length = 60,
+    }
+  }
+}
