21577c9c6b66c79074eb08c75d9efe395ee6de5f
fabiooo4 <fabioirimie@gmail.com>
2024-09-06 14:49:16 +0200

    changed lualine filename

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,6 +3,13 @@ return {
   config = function()
     require("lualine").setup({
       sections = {
+        lualine_c = {
+          {
+            "filename",
+            file_status = true,
+            path = 4,
+          },
+        },
         lualine_x = {
           {
             "datetime",
