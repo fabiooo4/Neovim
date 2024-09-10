df6bd6fd0700b0c43eb2ec7689cd3e30969b3ee4
fabiooo4 <fabioirimie@gmail.com>
2024-09-10 19:02:35 +0200

    remap <C-w> to <C-Backspace>

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -16,6 +16,9 @@ vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- Remap insert mode ctrl-w to ctrl-backspace
+vim.keymap.set("i", "<C-H>", "<C-W>", { noremap = true })
+
 -- Pane navigation
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
@@ -24,31 +27,31 @@ vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 
 -- Execute the binary with the same name as the current file
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "asm,c,cpp",
-  command = [[
+	pattern = "asm,c,cpp",
+	command = [[
     nnoremap <leader><F5> :TermExec cmd='./%:r'<CR> <C-w>ji
   ]],
 })
 
 -- Neovide
 if vim.g.neovide then
-  vim.keymap.set("n", "<C-v>", '"+p')             -- Paste normal mode
-  vim.keymap.set("v", "<C-v>", '"+p')             -- Paste visual mode
-  vim.keymap.set("c", "<C-v>", "<C-R>+")          -- Paste command mode
-  vim.keymap.set("i", "<C-v>", '<ESC>"+pla')      -- Paste insert mode
-  vim.keymap.set("t", "<C-v>", '<C-\\><C-n>"+pi') -- Paste terminal mode
+	vim.keymap.set("n", "<C-v>", '"+p') -- Paste normal mode
+	vim.keymap.set("v", "<C-v>", '"+p') -- Paste visual mode
+	vim.keymap.set("c", "<C-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<C-v>", '<ESC>"+pla') -- Paste insert mode
+	vim.keymap.set("t", "<C-v>", '<C-\\><C-n>"+pi') -- Paste terminal mode
 
-  -- Scale neovide
-  vim.g.neovide_scale_factor = 1.0
-  local change_scale_factor = function(delta)
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-  end
-  vim.keymap.set("n", "<C-=>", function()
-    change_scale_factor(1.25)
-  end)
-  vim.keymap.set("n", "<C-->", function()
-    change_scale_factor(1 / 1.25)
-  end)
+	-- Scale neovide
+	vim.g.neovide_scale_factor = 1.0
+	local change_scale_factor = function(delta)
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+	end
+	vim.keymap.set("n", "<C-=>", function()
+		change_scale_factor(1.25)
+	end)
+	vim.keymap.set("n", "<C-->", function()
+		change_scale_factor(1 / 1.25)
+	end)
 end
 
 -- Paste with C-v
