6232225ebf5a6683a2f08156b75df146b88934ed
fabiooo4 <fabioirimie@gmail.com>
2024-09-25 17:56:34 +0200

    disabled copilot on startup

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
-  "github/copilot.vim",
-  config = function()
-    vim.cmd [[imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")]]
-  end
+	"github/copilot.vim",
+	config = function()
+		vim.cmd(":Copilot disable")
+		vim.cmd([[imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")]])
+	end,
 }
