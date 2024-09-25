5049fdca0c05f7c0956fba2a192fd571e385d24a
fabiooo4 <fabioirimie@gmail.com>
2024-09-25 20:15:22 +0200

    added new lsp keybinds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,7 +19,7 @@ return {
 					"lua_ls",
 					"html",
 					"cssls",
-          "denols",
+					"denols",
 					"jsonls",
 					"marksman",
 					"cssls",
@@ -39,27 +39,60 @@ return {
 			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- setup lsp
-			lspconfig.clangd.setup({ capabilities = capabilities })
-			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.html.setup({ capabilities = capabilities })
-			lspconfig.cssls.setup({ capabilities = capabilities })
-			lspconfig.denols.setup({ capabilities = capabilities })
-			lspconfig.jsonls.setup({ capabilities = capabilities })
-			lspconfig.marksman.setup({ capabilities = capabilities })
-			lspconfig.glsl_analyzer.setup({ capabilities = capabilities })
-			lspconfig.svelte.setup({ capabilities = capabilities })
-      lspconfig.tailwindcss.setup({ capabilities = capabilities })
+			-- Use an on_attach function to only map the following keys
+			-- after the language server attaches to the current buffer
+			local on_attach = function(client, bufnr)
+				local function buf_set_keymap(...)
+					vim.api.nvim_buf_set_keymap(bufnr, ...)
+				end
 
-			-- keybinds
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			-- convert to lua
-			-- vim.cmd([[
-			--      map <leader>d :lua vim.diagnostic.open_float(0, {scope="line"})<CR>
-			--    ]])
-			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {})
+				-- setup lsp
+				--[[ lspconfig.clangd.setup({ capabilities = capabilities })
+				lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+				lspconfig.lua_ls.setup({ capabilities = capabilities })
+				lspconfig.html.setup({ capabilities = capabilities })
+				lspconfig.cssls.setup({ capabilities = capabilities })
+				lspconfig.denols.setup({ capabilities = capabilities })
+				lspconfig.jsonls.setup({ capabilities = capabilities })
+				lspconfig.marksman.setup({ capabilities = capabilities })
+				lspconfig.glsl_analyzer.setup({ capabilities = capabilities })
+				lspconfig.svelte.setup({ capabilities = capabilities })
+				lspconfig.tailwindcss.setup({ capabilities = capabilities }) ]]
+
+				-- keybinds
+				local opts = { noremap = true, silent = true }
+				buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+				vim.keymap.set({"n", "v"}, "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+				buf_set_keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+				buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+				buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+				buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+				buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+			end
+
+			local servers = {
+				"clangd",
+				"rust_analyzer",
+				"lua_ls",
+				"html",
+				"cssls",
+				"denols",
+				"jsonls",
+				"marksman",
+				"glsl_analyzer",
+				"svelte",
+				"tailwindcss",
+			}
+
+			for _, lsp in ipairs(servers) do
+				lspconfig[lsp].setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+					flags = {
+						debounce_text_changes = 150,
+					},
+				})
+			end
 		end,
 	},
 }
