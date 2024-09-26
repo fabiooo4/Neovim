6b5523763819edb0f89f183cea5c6e8f7d1f605f
fabiooo4 <fabioirimie@gmail.com>
2024-09-26 17:03:57 +0200

    updated lsp keybinds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -62,12 +62,15 @@ return {
 				-- keybinds
 				local opts = { noremap = true, silent = true }
 				buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-				vim.keymap.set({"n", "v"}, "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+				vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 				buf_set_keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 				buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 				buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 				buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
 				buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+				buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+				buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+				buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 			end
 
 			local servers = {
