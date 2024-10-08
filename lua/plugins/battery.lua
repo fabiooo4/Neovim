34be29cf4558213247f237527e9616797bf3155b
fabiooo4 <fabioirimie@gmail.com>
2024-10-08 14:12:42 +0200

    added battery status in lualine

---
+++ lua/plugins/battery.lua
@@ -0,0 +1,16 @@
+return {
+	"justinhj/battery.nvim",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local battery = require("battery")
+		battery.setup({
+			update_rate_seconds = 60, -- Number of seconds between checking battery status
+			show_status_when_no_battery = true, -- Don't show any icon or text when no battery found (desktop for example)
+			show_plugged_icon = false, -- If true show a cable icon alongside the battery icon when plugged in
+			show_unplugged_icon = true, -- When true show a diconnected cable icon when not plugged in
+			show_percent = true, -- Whether or not to show the percent charge remaining in digits
+			vertical_icons = true, -- When true icons are vertical, otherwise shows horizontal battery icon
+			multiple_battery_selection = "max", -- Which battery to choose when multiple found. "max" or "maximum", "min" or "minimum" or a number to pick the nth battery found (currently linux acpi only)
+		})
+	end,
+}
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,32 +1,36 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      sections = {
-        lualine_c = {
-          {
-            "filename",
-            file_status = true,
-            path = 4,
-          },
-        },
-        lualine_x = {
-          {
-            "datetime",
-            style = "%H:%M",
-          },
-        },
-        lualine_y = { "filetype" },
-        lualine_z = { "progress", "location" },
-      },
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		local function nvimbattery()
+			return require("battery").get_status_line()
+		end
 
-      options = {
-        theme = "auto",
-      },
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						file_status = true,
+						path = 4,
+					},
+				},
+				lualine_x = {
+					{
+						"datetime",
+						style = "%H:%M",
+					},
+					nvimbattery
+				},
+				lualine_y = { "filetype" },
+				lualine_z = { "progress", "location" },
+			},
 
-      extensions = {
-        "neo-tree",
-      },
-    })
-  end,
+			extensions = {
+				"neo-tree",
+			},
+		})
+	end,
 }
