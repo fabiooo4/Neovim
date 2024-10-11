430054804a97744b51df4cb50e8f96b2999c7ecb
fabiooo4 <fabioirimie@gmail.com>
2024-10-11 10:51:27 +0200

    removed inkscape support for latex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -26,11 +26,6 @@ return {
       endif
 
       nmap <leader>tc :VimtexTocToggle<CR>
-
-      " Inkscape integration by Gilles Castell:
-      " https://github.com/gillescastel/
-      inoremap <C-f> <Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>
-      nnoremap <C-f> : silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>
     ]])
 	end,
 }
