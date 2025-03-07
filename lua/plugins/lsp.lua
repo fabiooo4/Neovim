60cdae562a69f6a108825b793cc6cd7507dfa1ff
fabiooo4 <fabioirimie@gmail.com>
2025-03-07 22:56:32 +0100

    added lsp support to telescope

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -71,7 +71,6 @@ return {
         vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
         buf_set_keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
         buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-        buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
         buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
         buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
         buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,8 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>f", builtin.find_files, {})
       vim.keymap.set("n", "<leader>w", builtin.live_grep, {})
+      vim.keymap.set("n", "gr", builtin.lsp_references, {})
+      vim.keymap.set("n", "ds", builtin.lsp_document_symbols, {})
     end,
   },
   {
