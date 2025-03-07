0a441b064e6dd95e22afe8f51164d7062344ba8f
fabiooo4 <fabioirimie@gmail.com>
2025-03-08 00:09:28 +0100

    added lsp folds

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,6 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    update_debounce = 0,
+  }
+}
--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,6 +1,6 @@
 return {
   "karb94/neoscroll.nvim",
-  config = function ()
-    require('neoscroll').setup({})
-  end
+  opts = {
+    duration_multiplier = 0.5,
+  }
 }
