ed0cecdffcfc3fc8c3ff2523d900428fb1d3d358
fabiooo4 <fabioirimie@gmail.com>
2025-03-08 17:18:42 +0100

    changed copilot keymaps

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,11 @@ return {
 	"github/copilot.vim",
 	config = function()
 		vim.cmd(":Copilot disable")
-		vim.cmd([[imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")]])
+		vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+		})
+		vim.g.copilot_no_tab_map = true
+		vim.keymap.set("i", "<M-l>", "<Plug>(copilot-accept-word)")
 	end,
 }
