ea04e074e46d80d61fc9f80371218b7f9acdc99c
fabiooo4 <fabioirimie@gmail.com>
2025-03-26 22:12:19 +0100

    fixed conflicting keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
       vim.keymap.set("n", "<leader>f", builtin.find_files, {})
       vim.keymap.set("n", "<leader>w", builtin.live_grep, {})
       vim.keymap.set("n", "gr", builtin.lsp_references, {})
-      vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, {})
+      vim.keymap.set("n", "<leader>gs", builtin.lsp_document_symbols, {})
     end,
   },
   {
