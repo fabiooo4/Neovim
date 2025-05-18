f27231fcbb84a2acefed00d91ddd93f426c1e780
fabiooo4 <fabioirimie@gmail.com>
2025-05-19 01:27:15 +0200

    added sql table viewer

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,116 +1,116 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"hrsh7th/cmp-nvim-lsp-signature-help",
-	},
-	{
-		"hrsh7th/cmp-calc",
-	},
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+  },
+  {
+    "hrsh7th/cmp-calc",
+  },
   {
     "onsails/lspkind.nvim",
   },
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			require("luasnip").config.setup({
-				enable_autosnippets = true,
-			})
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      require("luasnip").config.setup({
+        enable_autosnippets = true,
+      })
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-			cmp.setup({
-				require("luasnip.loaders.from_vscode").lazy_load(),
+      cmp.setup({
+        require("luasnip.loaders.from_vscode").lazy_load(),
 
-				-- load snippets from directory
-				require("luasnip.loaders.from_vscode").lazy_load({
-					paths = { "./lua/snippets" },
-				}),
-				require("luasnip.loaders.from_lua").load({ paths = "./lua/snippets" }),
+        -- load snippets from directory
+        require("luasnip.loaders.from_vscode").lazy_load({
+          paths = { "./lua/snippets" },
+        }),
+        require("luasnip.loaders.from_lua").load({ paths = "./lua/snippets" }),
 
-				snippet = {
-					-- REQUIRED - you must specify a snippet engine
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				window = {
-					-- change window appearance
-					-- completion = cmp.config.window.bordered(),
-					-- documentation = cmp.config.window.bordered(),
-				},
-				formatting = {
-					format = function(entry, vim_item)
-						if vim.tbl_contains({ "path" }, entry.source.name) then
-							local icon, hl_group =
-								require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
-							if icon then
-								vim_item.kind = icon
-								vim_item.kind_hl_group = hl_group
-								return vim_item
-							end
-						end
-						return require("lspkind").cmp_format({ with_text = true })(entry, vim_item)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() and vim.bo.filetype ~= "tex" then
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          -- change window appearance
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            if vim.tbl_contains({ "path" }, entry.source.name) then
+              local icon, hl_group =
+                  require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
+              if icon then
+                vim_item.kind = icon
+                vim_item.kind_hl_group = hl_group
+                return vim_item
+              end
+            end
+            return require("lspkind").cmp_format({ with_text = true })(entry, vim_item)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() and vim.bo.filetype ~= "tex" then
               cmp.confirm({
                 select = true,
               })
-						elseif luasnip.expand_or_locally_jumpable(1) then
-							luasnip.expand_or_jump(1)
-						else
-							fallback()
-						end
-					end),
+            elseif luasnip.expand_or_locally_jumpable(1) then
+              luasnip.expand_or_jump(1)
+            else
+              fallback()
+            end
+          end),
 
-					["<A-j>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.locally_jumpable(1) then
-							luasnip.jump(1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
+          ["<A-j>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
 
-					["<A-k>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
+          ["<A-k>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
 
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-g>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-c>"] = cmp.mapping.abort(),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-g>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-c>"] = cmp.mapping.abort(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
           { name = "nvim_lsp_signature_help" }, -- function arg popups while typing
-					{ name = "luasnip" }, -- For luasnip users.
-					{ name = "calc" }, -- Calculates any expression right away
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+          { name = "luasnip" },                 -- For luasnip users.
+          { name = "calc" },                    -- Calculates any expression right away
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
 }
