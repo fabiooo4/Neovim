f38bb6e950f2262a5ca624a7ffa60cec2a42ae5f
fabiooo4 <fabioirimie@gmail.com>
2026-03-08 16:09:50 +0100

    feat: overridden vim.notify() with fidget.notify()

--- lua/plugins/notifications.lua
+++ lua/plugins/notifications.lua
@@ -1,10 +1,11 @@
 return {
-  "j-hui/fidget.nvim",
-  opts = {
-    notification = {
-      window = {
-        winblend = 0,        -- Background color opacity in the notification window
-      },
-    }
-  },
+	"j-hui/fidget.nvim",
+	opts = {
+		notification = {
+			override_vim_notify = true,
+			window = {
+				winblend = 0, -- Background color opacity in the notification window
+			},
+		},
+	},
 }
