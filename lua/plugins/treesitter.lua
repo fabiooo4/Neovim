2d3b38484b3640583c01ccad392c487a6111fee6
fabiooo4 <fabioirimie@gmail.com>
2026-08-08 22:04:50 +0200

    feat: added treesitter autoinstall script

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,23 +6,37 @@ return {
 	config = function()
 		require("nvim-treesitter").install({
 			"rust",
+			"c",
 			"javascript",
 			"lua",
 			"java",
+			"gitcommit",
+			"git_rebase",
+			"diff",
 		})
 
 		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "<filetype>" },
-			callback = function()
-				-- Highlight
-				vim.treesitter.start()
+			callback = function(ev)
+				local lang = vim.treesitter.language.get_lang(ev.match)
+				local available_langs = require("nvim-treesitter").get_available()
+				local is_available = vim.tbl_contains(available_langs, lang)
+				if is_available then
+					local installed_langs = require("nvim-treesitter").get_installed()
+					local installed = vim.tbl_contains(installed_langs, lang)
+					if not installed then
+						require("nvim-treesitter").install(lang):wait()
+					end
 
-				-- Folds
-				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-				vim.wo[0][0].foldmethod = "expr"
+					-- Highlights
+					vim.treesitter.start()
 
-				-- Indentation
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					-- Folds
+					vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.wo[0][0].foldmethod = "expr"
+
+					-- Indentation
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
 			end,
 		})
 	end,
